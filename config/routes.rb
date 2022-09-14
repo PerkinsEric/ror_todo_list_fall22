c47954a9580e6be9eef517aa904465a54aea913e
Eric Perkins <ericdperkins7@gmail.com>
2022-09-14 16:25:31 -0600

    todos backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
-    resources :lists
+    resources :lists do
+      resources :todos
+    end
   end
   # Defines the root path route ("/")
   # root "articles#index"
